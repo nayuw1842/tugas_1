d5f5e56211b244d53ad89f4d9acc4aef5efed317
ayaapple <azizahnurinayahputrim@gmail.com>
2026-07-20 16:44:35 +0800

    feat: Added Studi Kasus 2-4

---
+++ StudiKasus3.js
@@ -0,0 +1,16 @@
+const hero = [
+"Layla",
+"Ling",
+"Franco",
+"Fanny",
+"Miya",
+"Eudora"
+];
+
+for (let i = 0; i < hero.length; i++) {
+    if (hero[i][0] === "F") {
+        console.log(`${hero[i]} -> Hero Assasin/Tank Favorit`);
+    } else {
+        console.log(`${hero[i]} -> Hero Biasa`);
+    }
+}
